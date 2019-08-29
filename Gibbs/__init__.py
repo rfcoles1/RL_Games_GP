0e6cbeee3b29388092ad60a1a29d6c5b153915dc
rcoles <rfcoles@hotmail.co.uk>
2019-08-29 14:48:16 -0400

    update __init__ files

--- Gibbs/__init__.py
+++
@@ -1,1 +0,0 @@
-from GP_Games.Gibbs.phase_env import PhaseEnv
--- __init__.py
+++ __init__.py
@@ -3,31 +3,48 @@ from gym.envs.registration import register
 
 #---------------------------------------------#
 
+
 register(
     id = 'GP_Water-v0',
-    entry_point = 'GP_Games.Boiling.boil_env_v0:HeatEnv',
+    entry_point = 'GP_Games.Boiling.V0.boil_env:HeatEnv',
+    max_episode_steps = 100,
+)
+
+
+register(
+    id = 'GP_Water-v1',
+    entry_point = 'GP_Games.Boiling.V1.boil_env:HeatEnv',
+    max_episode_steps = 100,
+)
+ 
+
+register(
+    id = 'GP_Water-v2',
+    entry_point = 'GP_Games.Boiling.V2.boil_env:HeatEnv',
     max_episode_steps = 100,
 )
 
 register(
-    id = 'GP_Water-v1',
-    entry_point = 'GP_Games.Boiling.boil_env_v1:HeatEnv',
+    id = 'GP_Water-v3',
+    entry_point = 'GP_Games.Boiling.V3.boil_env:HeatEnv',
     max_episode_steps = 100,
 )
 
 #---------------------------------------------#
 
+"""
 register(
     id = 'GP_Func-v0',
     entry_point = 'GP_Games.GP_Function.functions_env:FuncEnv',
     max_episode_steps = 100,
 )
+"""
 
 #---------------------------------------------#
 
 register(
     id = 'GP_Phase-v0',
-    entry_point = 'GP_Games.Gibbs.phase_env:PhaseEnv',
+    entry_point = 'GP_Games.Gibbs.V0.phase_env:PhaseEnv',
     max_episode_steps = 25,
 )
 
